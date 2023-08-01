80431bb4395ecc6f59e5c17b34c172a397fa031f
Howie Nguyen <nguyen.howie2010@gmail.com>
2023-08-01 13:02:29 -0700

    added the new loading states

--- src/feature/TodoList/todoSelector.ts
+++ src/feature/TodoList/todoSelector.ts
@@ -2,10 +2,10 @@ import { RootState } from "./store";
 import { createSelector } from "@reduxjs/toolkit";
 
 export const selectTodos = (state: RootState) => state.todo.todos;
-
+export const selectLoadingStates = (state: RootState) =>
+	state.todo.loadingStates;
+export const selectError = (state: RootState) => state.todo.error;
 // Example of a more complex selector to filter completed todos
 export const selectCompletedTodos = createSelector([selectTodos], (todos) =>
 	todos.filter((todo) => todo.completed),
 );
-
-
--- src/feature/TodoList/todoSlice.ts
+++ src/feature/TodoList/todoSlice.ts
@@ -6,20 +6,30 @@ import TodoListRepoImpl, { TodoListRepo } from "./domain/TodoListRepo";
 //the todo array
 interface TodoState {
 	todos: Todo[];
+	loadingStates: {
+		fetch: "idle" | "pending" | "succeeded" | "failed";
+		save: "idle" | "pending" | "succeeded" | "failed";
+		update: "idle" | "pending" | "succeeded" | "failed";
+		remove: "idle" | "pending" | "succeeded" | "failed";
+	};
+	error: string | null;
 }
-
-
 
 // Create an instance of TodoListRepoImpl to use as the implementation
 const todoListRepo: TodoListRepo = new TodoListRepoImpl();
-// this is explicit typing 
+// this is explicit typing
 
 // the intial state of the todo array
 const initialState: TodoState = {
 	todos: [],
+	loadingStates: {
+		fetch: "idle",
+		save: "idle",
+		update: "idle",
+		remove: "idle",
+	},
+	error: null,
 };
-
-
 
 export const fetchTodos = createAsyncThunk("todo/fetchTodos", async () => {
 	return await todoListRepo.getTodos();
@@ -47,7 +57,7 @@ export const removeTodo = createAsyncThunk(
 	},
 );
 
-//the todo list with its functions
+// the todo list with its functions
 // generally it creats a slice of the redux store.
 // I gave it a name, the intial state and an object defining several reducers
 export const todoSlice = createSlice({
@@ -78,22 +88,61 @@ export const todoSlice = createSlice({
 
 	extraReducers: (builder) => {
 		builder
+			.addCase(fetchTodos.pending, (state) => {
+				state.loadingStates.fetch = "pending";
+			})
 			.addCase(fetchTodos.fulfilled, (state, action) => {
+				state.loadingStates.fetch = "succeeded";
 				state.todos = action.payload;
+				state.error = null;
+			})
+			.addCase(fetchTodos.rejected, (state, action) => {
+				state.loadingStates.fetch = "failed";
+				state.error = action.error.message || "An error occurred";
+			})
+
+			.addCase(saveTodo.pending, (state) => {
+				state.loadingStates.save = "pending";
 			})
 			.addCase(saveTodo.fulfilled, (state, action) => {
+				state.loadingStates.save = "succeeded";
 				state.todos.push(action.payload);
+				state.error = null;
+			})
+			.addCase(saveTodo.rejected, (state, action) => {
+				state.loadingStates.save = "failed";
+				state.error = action.error.message || "An error occurred";
+			})
+
+			.addCase(updateTodo.pending, (state) => {
+				state.loadingStates.update = "pending";
 			})
 			.addCase(updateTodo.fulfilled, (state, action) => {
+				state.loadingStates.update = "succeeded";
 				const index = state.todos.findIndex(
 					(todo) => todo.id === action.payload.id,
 				);
 				if (index !== -1) {
 					state.todos[index] = action.payload;
 				}
+				state.error = null;
+			})
+			.addCase(updateTodo.rejected, (state, action) => {
+				state.loadingStates.update = "failed";
+				state.error = action.error.message || "An error occurred";
+			})
+
+			.addCase(removeTodo.pending, (state) => {
+				state.loadingStates.remove = "pending";
 			})
 			.addCase(removeTodo.fulfilled, (state, action) => {
+				state.loadingStates.remove = "succeeded";
 				state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+				state.error = null;
+			})
+			.addCase(removeTodo.rejected, (state, action) => {
+				state.loadingStates.remove = "failed";
+				state.error = action.error.message || "An error occurred";
 			});
 	},
 });
