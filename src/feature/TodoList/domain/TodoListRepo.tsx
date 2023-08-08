d2ea27001a8aa8a4fdac2c8127f6c8aadf89821e
Howie Nguyen <nguyen.howie2010@gmail.com>
2023-08-07 19:10:40 -0700

    added the pwa modifications

--- src/feature/TodoList/domain/TodoListRepo.tsx
+++ src/feature/TodoList/domain/TodoListRepo.tsx
@@ -1,5 +1,6 @@
 import { Todo } from "./Todo";
-
+import db from "../utils/db";
+import Dexie from "dexie";
 // Define the interface for the TodoListRepo
 export interface TodoListRepo {
 	getTodos(): Promise<Todo[]>;
@@ -13,44 +14,58 @@ export interface TodoListRepo {
 //use the array within in the todolistrepoimpl
 //
 class TodoListRepoImpl implements TodoListRepo {
-	private todos: Todo[]; // The array should be inside the class
-
 	constructor() {
-		this.todos = [
-			{ id: 1, text: "Buy groceries", completed: false },
-			{ id: 2, text: "Do laundry", completed: false },
-			{ id: 3, text: "Walk the dog", completed: false },
-		];
+		// You can also add data here to prepopulate the database, if needed
+		void db.open(); // Open the database
 	}
+
 	async getTodos(): Promise<Todo[]> {
-		await new Promise((resolve) => setTimeout(resolve, 1000)); // Delay of 1 second
-		return Promise.resolve([...this.todos]); // Copy the array to ensure it is extensible
+		try {
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+			// Use the Dexie library to fetch data from the 'todos' table in IndexedDB
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+			const todos = await db.todos.toArray();
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+
+			return todos;
+		} catch (error) {
+			console.error("Error while fetching todos:", error);
+			throw new Error("Failed to fetch todos");
+		}
 	}
 
 	async addTodo(todo: Todo): Promise<Todo> {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-		if (todo.text.toLowerCase() === "apple") {
-			// assuming the attribute is 'name'
-			throw new Error("The item 'apple' is not correct.");
+		try {
+			// Use the Dexie library to add a new todo to the 'todos' table in IndexedDB
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+			const newTodoId = await db.todos.add(todo);
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			return { ...todo, id: newTodoId };
+		} catch (error) {
+			console.error("Error while adding a todo:", error);
+			throw new Error("Failed to add todo");
 		}
-		const newTodo = { ...todo };
-
-		this.todos.push(newTodo);
-		return Promise.resolve(newTodo);
 	}
 
 	async toggleTodo(todo: Todo): Promise<Todo> {
-		const index = this.todos.findIndex((t) => t.id === todo.id);
-		if (index !== -1) {
-			this.todos[index] = { ...todo };
-			return Promise.resolve(todo);
+		try {
+			// Use the Dexie library to update a todo in the 'todos' table in IndexedDB
+			await db.todos.update(todo.id, { completed: !todo.completed });
+			return { ...todo, completed: !todo.completed };
+		} catch (error) {
+			console.error("Error while toggling a todo:", error);
+			throw new Error("Failed to toggle todo");
 		}
-		return Promise.reject(new Error("Todo not found"));
 	}
 
 	async deleteTodo(todoId: number): Promise<void> {
-		this.todos = this.todos.filter((t) => t.id !== todoId);
-		return Promise.resolve();
+		try {
+			// Use the Dexie library to delete a todo from the 'todos' table in IndexedDB
+			await db.todos.delete(todoId);
+		} catch (error) {
+			console.error("Error while deleting a todo:", error);
+			throw new Error("Failed to delete todo");
+		}
 	}
 }
 
